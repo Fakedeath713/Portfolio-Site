254af2d6c0410e77efed30279a75e0813e11a73c
Fakedeath713 <azizcan.fb.7131@hotmail.com>
2025-11-24 02:46:47 +0300

    Images added - TO-DO Updated

--- data/videos.js
+++ data/videos.js
@@ -1,8 +1,17 @@
 // src/data/videos.js
 
 export const videos = [
-  { src: '/videos/bag2_NON.mp4', title: 'Koleksiyon Tanıtımı 1', orientation: 'horizontal' },
-  { src: '/videos/dress_NON.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
-  { src: '/videos/dress1_NON.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
-  { src: '/videos/shoes_NON.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
+  { src: '/videos/video1.mp4', title: 'Atölye Çalışmaları', orientation: 'vertical' },
+  { src: '/videos/video2.mp4', title: 'Atölye Çalışmaları', orientation: 'vertical' },
+  { src: '/videos/video0.mp4', title: 'Koleksiyon Tanıtımı 1', orientation: 'vertical' },
+  { src: '/videos/video3.mp4', title: 'Atölye Çalışmaları', orientation: 'vertical' },
+  { src: '/videos/video4.mp4', title: 'Atölye Çalışmaları', orientation: 'vertical' },
+  { src: '/videos/video5.mp4', title: 'Atölye Çalışmaları', orientation: 'vertical' },
+  { src: '/videos/video6.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
+  { src: '/videos/video7.mp4', title: 'Atölye Çalışmaları', orientation: 'vertical' },
+  { src: '/videos/video8.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
+  { src: '/videos/video9.mp4', title: 'Atölye Çalışmaları', orientation: 'vertical' },
+  { src: '/videos/video10.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
+  { src: '/videos/video11.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
+  { src: '/videos/video12.mp4', title: 'Atölye Çalışmaları', orientation: 'vertical' },
 ];
