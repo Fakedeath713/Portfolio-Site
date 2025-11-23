7bf5ac1a6b7ec1b1ea4582da214de66ecdd6ec75
Fakedeath713 <azizcan.fb.7131@hotmail.com>
2025-11-23 19:46:42 +0300

    TODO updated

--- data/videos.js
+++ data/videos.js
@@ -1,11 +1,8 @@
 // src/data/videos.js
 
 export const videos = [
-  { src: '/videos/bag.mp4', title: 'Koleksiyon Tanıtımı 1', orientation: 'horizontal' },
-  { src: '/videos/bag2.mp4', title: 'Tasarım Süreci', orientation: 'horizontal' },
-  { src: '/videos/bag3.mp4', title: 'Defile Görüntüleri', orientation: 'vertical' },
-  { src: '/videos/bag4.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
-  { src: '/videos/dress.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
-  { src: '/videos/dress1.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
-  { src: '/videos/shoes.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
+  { src: '/videos/bag2_NON.mp4', title: 'Koleksiyon Tanıtımı 1', orientation: 'horizontal' },
+  { src: '/videos/dress_NON.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
+  { src: '/videos/dress1_NON.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
+  { src: '/videos/shoes_NON.mp4', title: 'Atölye Çalışmaları', orientation: 'horizontal' },
 ];
